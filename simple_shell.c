1f46a754276cde98c61066c399e5c7f14478eec1
meyota <meyota08@gmail.com>
2022-09-25 04:03:16 -0700

    shell

--- simple_shell.c
+++ simple_shell.c
@@ -1,68 +1,73 @@
 #include "shell.h"
 
-/* global variable for ^C handling */
-unsigned int sig_flag;
-
 /**
- * sig_handler - handles ^C signal interupt
- * @uuv: unused variable (required for signal function prototype)
+ * free_data - frees data structure
  *
- * Return: void
+ * @datash: data structure
+ * Return: no return
  */
-static void sig_handler(int uuv)
+void free_data(data_shell *datash)
 {
-	(void) uuv;
-	if (sig_flag == 0)
-		_puts("\n$ ");
-	else
-		_puts("\n");
+	unsigned int i;
+
+	for (i = 0; datash->_environ[i]; i++)
+	{
+		free(datash->_environ[i]);
+	}
+
+	free(datash->_environ);
+	free(datash->pid);
 }
 
 /**
- * main - main function for the shell
- * @argc: number of arguments passed to main
- * @argv: array of arguments passed to main
- * @environment: array of environment variables
+ * set_data - Initialize data structure
  *
- * Return: 0 or exit status, or ?
+ * @datash: data structure
+ * @av: argument vector
+ * Return: no return
  */
-int main(int argc __attribute__((unused)), char **argv, char **environment)
+void set_data(data_shell *datash, char **av)
 {
-	size_t len_buffer = 0;
-	unsigned int is_pipe = 0, i;
-	vars_t vars = {NULL, NULL, NULL, 0, NULL, 0, NULL};
+	unsigned int i;
 
-	vars.argv = argv;
-	vars.env = make_env(environment);
-	signal(SIGINT, sig_handler);
-	if (!isatty(STDIN_FILENO))
-		is_pipe = 1;
-	if (is_pipe == 0)
-		_puts("$ ");
-	sig_flag = 0;
-	while (getline(&(vars.buffer), &len_buffer, stdin) != -1)
+	datash->av = av;
+	datash->input = NULL;
+	datash->args = NULL;
+	datash->status = 0;
+	datash->counter = 1;
+
+	for (i = 0; environ[i]; i++)
+		;
+
+	datash->_environ = malloc(sizeof(char *) * (i + 1));
+
+	for (i = 0; environ[i]; i++)
 	{
-		sig_flag = 1;
-		vars.count++;
-		vars.commands = tokenize(vars.buffer, ";");
-		for (i = 0; vars.commands && vars.commands[i] != NULL; i++)
-		{
-			vars.av = tokenize(vars.commands[i], "\n \t\r");
-			if (vars.av && vars.av[0])
-				if (check_for_builtins(&vars) == NULL)
-					check_for_path(&vars);
-			free(vars.av);
-		}
-		free(vars.buffer);
-		free(vars.commands);
-		sig_flag = 0;
-		if (is_pipe == 0)
-			_puts("$ ");
-		vars.buffer = NULL;
+		datash->_environ[i] = _strdup(environ[i]);
 	}
-	if (is_pipe == 0)
-		_puts("\n");
-	free_env(vars.env);
-	free(vars.buffer);
-	exit(vars.status);
+
+	datash->_environ[i] = NULL;
+	datash->pid = aux_itoa(getpid());
+}
+
+/**
+ * main - Entry point
+ *
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: 0 on success.
+ */
+int main(int ac, char **av)
+{
+	data_shell datash;
+	(void) ac;
+
+	signal(SIGINT, get_sigint);
+	set_data(&datash, av);
+	shell_loop(&datash);
+	free_data(&datash);
+	if (datash.status < 0)
+		return (255);
+	return (datash.status);
 }
